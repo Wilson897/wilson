0a63526404d21e8255c9d8f6a7304a22371c3b84
wilson897 <cb.wilson.perez.a@upds.net>
2022-02-23 11:11:39 -0400

    s

--- java.js
+++ java.js
@@ -1,15 +1,7 @@
 
-for (let i = 1; i< 101; i++) {
-    if (i % 15 == 0) {
-         console.log('FIZZBUZZ');
-     }
-    else if (i % 3 == 0) {
-        console.log('FIZZ');
-    }
-    else if (1 % 5 == 0) {
-        console.log('BUZZ');
-    }
-    else {
-         console.log(i);
-    }
+for(var i=1;i<=100;i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
 }
